1a354ba055c9b9f3f81c23cbdd5a0280be08f729
Nonthanan Simpree <nonthanansimpree@gmail.com>
2026-02-25 17:59:04 +0700

    Integrate NativeWind (Tailwind) into Expo app
    Add NativeWind/Tailwind setup and update app UI to use Tailwind classes. Introduces babel.config.js, metro.config.js, global.css, nativewind-env.d.ts and tailwind.config.js to enable nativewind in the Expo project. Update app/_layout.tsx to import global.css and replace the previous Home screen (StyleSheet) with a Tailwind-styled component in app/index.tsx (renamed to App and using Tailwind classes and a new react-logo image). package.json updated to include required dependencies and tsconfig adjusted for the setup.

--- react-native-start/app/index.tsx
+++ react-native-start/app/index.tsx
@@ -1,25 +1,16 @@
-import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Button, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-export default function Home() {
-    return(
-        <ScrollView style={ style.container}>
-            <Text>Welcome Non!!</Text>
-                <Image source={require('../assets/images/icon.png')} style={{ width: 200, height: 200}}/>
-            <TouchableOpacity style={ style.button}>
-                <Text>กด</Text>
-            </TouchableOpacity>
-        </ScrollView>
-    )
+export default function App() {
+  return (
+    <View className="bg-blue-500 flex-1 justify-center items-center">
+      <View className=" bg-white border rounded-xl p-16 flex flex-col gap-4">
+        <Text className="text-blue-900 text-lg font-bold">
+          Hello Tailwind in React Native!
+        </Text>
+        <Image source={require('../assets/images/react-logo.png')} style={{ width: 200, height: 200}}/>
+        <TextInput className=" border bg-blue-100 w-{200px} rounded-lg p-2"/>
+        <TouchableOpacity className="bg-blue-900 w-{200px} rounded-lg p-2 flex justify-center items-center"><Text className="text-white">กด</Text></TouchableOpacity>
+      </View>
+    </View>
+  );
 }
-
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 24,
-        backgroundColor: '#eaeaea'
-    },
-    button: {
-        width: 200,
-        backgroundColor: '#3B82F6',
-    }
-})
